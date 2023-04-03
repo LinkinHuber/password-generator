b05c949171cfcea6768330952f7b01c0c81743c9
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-04-03 17:37:20 -0500

    completely finished the JavaScript file and got the password generator working how its supposed to

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # password-generator
+
+## what was the goal of this project?
+
+ - The goal of this project was to create a 
+
+## link to the production "live" version of the code
+
+- https://linkinhuber.github.io/password-generator/
+
+## screenshot of webpage
+
+![Screenshot of home page](./images/Screenshot%202023-04-03%20173434.png)
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,4 @@
 var generateBtn = document.querySelector("#generate");
-
-//Variables used in the generatePassword function
 var passwordLength 
 var characters = [];
 
@@ -10,45 +8,46 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["!", "$", "%", "&", "@", "?", "*", "#", "%", "(", ")", "+", "-", "=", ",", ".", "/", ":", ";", "<", ">", "[", "]", "^", "_", "{", "}"];
 
-//Variables for passwordCriteria function
-var includeLowercase
-var includeUppercase
-var includeNumeric
-var includeSpecialCharacters
+//Function that runs through the prompts one by one to see what criteria you want included in your password. The first one determines how many characters you want it to be and the rest determine which character classes will be included  
+function passwordCriteria() { 
 
-//Criteria questions to see how you want to create your password
-function passwordCriteria() {
   passwordLength = parseInt(prompt("How long should the password be?\nSelect a value between 8 and 128"));
-  includeLowercase = confirm("Do you want to include lowercase letters?");
-    characters = characters.concat(lowercase);
-  includeUppercase = confirm("Do you want to include uppercase letters?");
-    characters = characters.concat(uppercase);
-  includeNumeric = confirm("Do you want to include numeric characters?");
-    characters = characters.concat(numeric);
-  includeSpecialCharacters = confirm("Lastly, do you want to include special characters?");
-    characters = characters.concat(specialCharacters);
+    if (passwordLength < 8 || passwordLength > 128) //if you put in a number less than 8 or greater than 128 your response will be invalid
+    return alert("Please Try Again");
+//as you go through the prompts it only includes that character class if the answer is true and combines it with the other true reponses (concat) as yop go down the list. Otherwise, if false it doesnt include that character class in your password
+  includeLowercase = confirm("Do you want to include lowercase letters?")
+    if (includeLowercase === true) characters = characters.concat(lowercase); 
+  
+  includeUppercase = confirm("Do you want to include uppercase letters?") 
+    if (includeUppercase === true) characters = characters.concat(uppercase);
+    
+  includeNumeric = confirm("Do you want to include numeric characters?") 
+    if (includeNumeric === true) characters = characters.concat(numeric);
+    
+  includeSpecialCharacters = confirm("Lastly, do you want to include special characters?") 
+    if (includeSpecialCharacters === true) characters = characters.concat(specialCharacters);      
 }
 
-//This function generates the password by throwing all of the selected criteria into a random generator that generates a passowrd specific to the amount of characters you want and the desired types of characters.
+//This function generates the password by throwing all of the selected criteria into a random generator that generates a passoword specific to the amount of characters you want and the desired types of characters... it then returns the password
 function generatePassword() {
 
  var password = "";
 
  for(var i = 0; i <passwordLength; i++){
    var randomIndex = Math.floor(Math.random() * characters.length); //Random generator right here 
-   password = password + characters[randomIndex];
+   password += characters[randomIndex];
  }
  return password;
 }
 
-//This function takes the password from #password imput and writes it out
+//This function takes the generated password and writes it out in the box on the page
 function writePassword() {
   var rightCriteria = generatePassword();
   var passwordText = document.querySelector("#password");
 
   if (rightCriteria) {
-    var newPassword = generatePassword();
-    passwordText.value = newPassword;
+    var yourPassword = generatePassword();
+    passwordText.value = yourPassword;
   } else {
     passwordText.value = "";
   }
